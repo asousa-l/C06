ed73b2d0dfb9dff37d346cd7712b8cf96a4be2b0
Andre Sousa lajas <asousa-l@c1r16s1.42lisboa.com>
2022-01-26 16:48:17 +0000

    a

---
+++ ex00/ft_print_program_name.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asousa-l <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/26 15:33:25 by asousa-l          #+#    #+#             */
+/*   Updated: 2022/01/26 15:37:33 by asousa-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdbool.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	char	a;
+
+	while (true)
+	{
+		a = *str;
+		if (a == '\0')
+		{
+			break ;
+		}
+		ft_putchar(a);
+		str++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	(void)	argv;
+
+	ft_putstr(argv[0]);
+	return (0);
+}
---
+++ ex03/ft_sort_params.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asousa-l <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/26 16:25:24 by asousa-l          #+#    #+#             */
+/*   Updated: 2022/01/26 16:47:44 by asousa-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	char	a;
+
+	while (true)
+	{
+		a = *str;
+		if (a == '\0')
+		{
+			break ;
+		}
+		ft_putchar(a);
+		str++;
+	}
+}
+
+void	ft_swap(int *a, int *b)
+{
+	int	c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+	return (*(unsigned char *)s1 - *(unsigned char *)s2);
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	j = 0;
+	if (argc > 1)
+	{
+		while (i < argc)
+		{
+			j = 0;
+			while (argv[i][j] != '\0')
+			{
+				write(1, &argv[i][j], 1);
+				j++;
+			}
+			write(1, "\n", 1);
+			i++;
+		}
+	}
+	return (0);
+}
+
