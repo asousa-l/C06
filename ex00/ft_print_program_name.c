5c44844d0388fd65734521823ce6308545e3be7e
Andre Sousa lajas <asousa-l@c1r16s1.42lisboa.com>
2022-01-27 00:05:12 +0000

    a

--- ex00/ft_print_program_name.c
+++ ex00/ft_print_program_name.c
@@ -6,7 +6,7 @@
 /*   By: asousa-l <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 15:33:25 by asousa-l          #+#    #+#             */
-/*   Updated: 2022/01/26 15:37:33 by asousa-l         ###   ########.fr       */
+/*   Updated: 2022/01/26 16:56:56 by asousa-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,8 +36,7 @@ void	ft_putstr(char *str)
 
 int	main(int argc, char **argv)
 {
-	(void)	argv;
-
+	(void) argv;
 	ft_putstr(argv[0]);
 	return (0);
 }
